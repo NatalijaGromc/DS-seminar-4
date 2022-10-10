6c70dcd1af116395143728d8e65339b0cff8e01d
natasha <gromc-natalija@rambler.ru>
2022-10-10 22:15:28 +0300

    дз1

--- DS1/Program.cs
+++ DS1/Program.cs
@@ -3,14 +3,18 @@
 // 2, 4 -> 16
 
 
+int Pow(int A, int B)
+{
+    int prod = 1;
+    for (int i = 0; i < B; i++)
+    prod = prod * A;
+    return prod;
+}
+
 int A,B;
 Console.Write($"Введите число A: ");
 int.TryParse(Console.ReadLine()!, out A);
 Console.Write($"Введите число B: ");
 int.TryParse(Console.ReadLine()!, out B);
 
-int prod = 1;
-for (int i = 0; i < B; i++)
-prod = prod * A;
-
-Console.Write($"Возведение числа {A} в степени {B} = {prod}");
+Console.Write($"Возведение числа {A} в степени {B} = {Pow (A,B)}");
