ae62f7d0d91b0e677ca3ec8e7b6f06564ec36187
natasha <gromc-natalija@rambler.ru>
2022-10-10 22:52:42 +0300

    дз3

---
+++ DS3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array;
+
+int[] CreateArray()
+{
+int N;
+string userEnter;
+int[] _array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"Введите элемент номер {i} ");
+    userEnter = Console.ReadLine()!;
+    N = int.Parse(userEnter);
+    _array[i] = N;
+}
+return _array;
+}
+
+array = CreateArray();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+Console.Write($"{array[i]} ");
+Console.Write("]");
