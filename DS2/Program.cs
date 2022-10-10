411b7758b338b619f9ddacfa4fcd1f8a75ed899e
natasha <gromc-natalija@rambler.ru>
2022-10-10 22:32:33 +0300

    дз2

---
+++ DS2/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int N,sum,num,temp;
+string userEnter;
+
+Console.Write ("Введите число N: ");
+userEnter = Console.ReadLine()!;
+N = int.Parse(userEnter);
+temp=N;
+sum=0;
+while (temp>0)
+{
+    num=temp%10;
+    sum=sum+num;
+    temp=temp/10;
+}
+{
+Console.Write($"сумма цифр {sum}");
+}
